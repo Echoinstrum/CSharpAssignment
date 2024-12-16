863731827301b2044d1563a050f027086a60a682
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2024-12-16 15:10:12 +0100

    Base structure of user data created

--- Business/Models/User.cs
+++ Business/Models/User.cs
@@ -8,6 +8,14 @@ namespace Business.Models
 {
     public class User
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; }
+        public string StreetAddress { get; set; } = null!;
+        public string PostalCode { get; set; }
 
+        public string PlaceOfResidence { get; set; } = null!;
     }
 }
