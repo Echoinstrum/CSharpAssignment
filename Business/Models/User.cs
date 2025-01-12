aea6fa266e6d1b810ef24388a989632dd01f4190
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-01-12 23:20:41 +0100

    Removed Guid from constructor. Unecessary "double use"

--- Business/Models/User.cs
+++ Business/Models/User.cs
@@ -18,9 +18,16 @@ namespace Business.Models
 
         public string PlaceOfResidence { get; set; } = null!;
 
+        /*
+         Minor refactoring with help from Chat GPT 4o here.
+         Previously i used Guid.NewGuid() both in the ield declaration and in the constructor.
+         It was unecessary use since the field declaration already had the Guid initialized.
+         Removed it so it didn't call it two times, just to overwrite the field declarations Guid. 
+         I didn't quite think that part through when i wrote the code.
+        */
+        
         public User(string firstName, string lastName, string email, string phoneNumber, string streetAddress, string postalCode, string placeOfResidence)
         {
-            Id = Guid.NewGuid();
             FirstName = firstName;
             LastName = lastName;
             Email = email;
