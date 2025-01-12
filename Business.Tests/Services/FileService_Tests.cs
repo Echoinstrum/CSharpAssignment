14a8bb5a576bb0879adb70b787bc6ed6e49f0fe5
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-01-12 22:18:52 +0100

    feature added a few basic tests for methods in userservice and fileservice, minor change to MenuService-switch

---
+++ Business.Tests/Services/FileService_Tests.cs
@@ -0,0 +1,33 @@
+﻿using Business.Models;
+using Business.Services;
+
+namespace Business.Tests.Services;
+
+public class FileService_Tests
+{
+    [Fact]
+    public void LoadUsersFromFile_ShouldReturnEmptyList__WhenFileIsMissingOrEmpty()
+    {
+
+        // Arrange
+        string filePath = "users.json";
+
+        // Act
+        if (File.Exists(filePath)) 
+        {
+            File.Delete("users.json");
+        }
+        var fileService = new FileService();
+
+        List<User> result = fileService.LoadUsersFromFile();
+        
+        // Assert
+        Assert.Empty(result);
+    }
+
+    /* 
+    Tried to also test the SaveUsersToFile method, validating saving correct data to the file. 
+    But i couldn't quite come around how to do it, atleast not in time for turning in the assignment. 
+    Gotta work more on that.
+    */
+}
